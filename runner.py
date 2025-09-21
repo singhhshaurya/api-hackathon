57604c33607430d846b9dfc805b0198b3419605f
singhhshaurya <shauryanarainsingh@gmail.com>
2025-09-22 02:48:20 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,4 +1,53 @@
 # import mapper, namaste_data_mapped from initialiser.
+import json
+from datetime import date
+
+# print(namaste_data_mapped)
+concept_map_index = {}
+
+concept_map = {
+    "resourceType": "ConceptMap",
+    "id": "namaste-to-tm2",
+    "name": "NAMASTEtoTM2",
+    "description": "This ConceptMap maps standardized NAMASTE Ayurveda diagnostic codes to corresponding ICD-11 TM2 Traditional Medicine codes for clinical interoperability.",
+    "status": "active",
+    "date":date.today().isoformat(),
+    "sourceUri": "http://namaste.org/codes",
+    "targetUri": "http://id.who.int/icd/release/11/2025-01/tm2",
+    "group": [
+        {
+            'element':[]
+        }
+    ]
+}
+
+def add_to_concept_map(namaste_entry, top_matches):
+
+    EXACT_TH = 0.7
+    RELATED_TH = 0.3
+    def _category_for_score(score): 
+        if score >= EXACT_TH:
+            return "exactMatch"
+        if score >= RELATED_TH:
+            return "relatedMatch"
+        return "noMatch"
+    
+    entry = {
+          "code": namaste_entry['code'],
+          "display": namaste_entry['title'],
+          "target": []
+    }
+    for i in top_matches:
+        p = {
+            'code':i['icd_code'],
+            'display':i['title'],
+            "equivalence": _category_for_score(i['similarity'])
+        }
+        entry['target'].append(p)
+    
+    return entry
+    
+
 
 
 def fetch_namaste_entry(namaste_code):
@@ -11,10 +60,19 @@ def fetch_namaste_entry(namaste_code):
 
 def namaste_to_icd(namaste_code):
     # fetch namaste entry first.
+    if namaste_code in concept_map_index:
+        index = concept_map_index[namaste_code]
+        print("DORA")
+        return concept_map['group'][0]['element'][index]
+        
     namaste_entry = fetch_namaste_entry(namaste_code)
+    
     if namaste_entry:
         top_matches = mapper.map_entry(namaste_entry)
-        return top_matches
+        top_matches = add_to_concept_map(namaste_entry, top_matches) # gives us in conceptmap format.
+        concept_map['group'][0]['element'].append(top_matches)
+        concept_map_index[namaste_code] = len(concept_map['group'][0]['element']) - 1
+        return top_matches # this is the ACTUAL icd codes data.
     else:
         return None
 
@@ -22,13 +80,10 @@ def namaste_to_icd(namaste_code):
 '''
 EXAMPLE INPUT: namaste_to_icd('AAB-3')
 EXAMPLE OUTPUT: 
-[{'icd_code': 'SP9Y',
-  'title': 'Other specified disorders affecting the whole body (TM2)',
-  'similarity': 1.0000001192092896},
- {'icd_code': 'SM1Y',
-  'title': 'Other specified oral cavity disorders (TM2)',
-  'similarity': 0.6810562014579773},
- {'icd_code': 'SK95',
-  'title': 'Rhinitis disorder (TM2)',
-  'similarity': 0.6641131639480591}]
+
+{'code': 'ABB-10', 'display': 'ushmadhikyam kevalapitta', 
+'target': [{'code': 'SP9Y', 'display': 'Other specified disorders affecting the whole body (TM2)', 'equivalence': 'exactMatch'}, 
+           {'code': 'SM87', 'display': 'Dysuria disorder (TM2)', 'equivalence': 'relatedMatch'}, 
+           {'code': 'SP5Y', 'display': 'Other specified febricity disorders (TM2)', 'equivalence': 'relatedMatch'}]
+}
 '''
