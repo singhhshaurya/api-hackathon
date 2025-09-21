7279d4050ac255e829e53374bc18543373589c2f
singhhshaurya <shauryanarainsingh@gmail.com>
2025-09-21 20:20:48 +0530

    Update model_initialiser.py

--- model_initialiser.py
+++ model_initialiser.py
@@ -6,8 +6,8 @@ with open("tm2_data_main.json", "r", encoding="utf-8") as f:
 with open("sanskrit-to-english.json", "r", encoding="utf-8") as f:
     alias = json.load(f)
 
-with open("namaste_data_main.json", "r", encoding="utf-8") as f:
-    namaste_list = json.load(f)
+with open("namaste_entries.json", "r", encoding="utf-8") as f:
+    namaste_data_mapped = json.load(f)
 
 #print(icd_list)
 mapper = Mapper(alias, icd_list)
